a8dd636b82842cc315ec675214f6c0a2f7ec5b1f
nemanjapu <nemanja.pu@gmail.com>
2020-10-21 22:40:16 +0200

    changes

--- Marik/Areas/Admin/Controllers/DashboardController.cs
+++ Marik/Areas/Admin/Controllers/DashboardController.cs
@@ -13,7 +13,7 @@ namespace Marik.Areas.Admin.Controllers
     public class DashboardController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        private static MailChimpManager mcManager = new MailChimpManager("7c9f307bfe083c6152698211349bf9a8-us20");
+        //private static MailChimpManager mcManager = new MailChimpManager("7c9f307bfe083c6152698211349bf9a8-us20");
 
         public DashboardController(IUnitOfWork unitOfWork)
         {
@@ -23,7 +23,7 @@ namespace Marik.Areas.Admin.Controllers
         // GET: Admin/Dashboard
         public async System.Threading.Tasks.Task<ActionResult> Index()
         {
-            var newsletterSubscribers =  await mcManager.Members.GetTotalItems("a0df8aa760", null).ConfigureAwait(false);
+            //var newsletterSubscribers =  await mcManager.Members.GetTotalItems("a0df8aa760", null).ConfigureAwait(false);
             var model = new DashboardViewModel()
             {
                 LatestContacts = _unitOfWork.Lead.GetAllLeads().Take(10).ToList(),
@@ -31,8 +31,8 @@ namespace Marik.Areas.Admin.Controllers
                 MostDownloadedProduct = _unitOfWork.Product.GetAllProducts().OrderByDescending(p => p.NumberOfDownloads).FirstOrDefault(),
                 NumberOfBlogs = _unitOfWork.Blog.GetActiveBlogs().Count(),
                 NumberOfContacts = _unitOfWork.Lead.GetAllLeads().Count(),
-                NumberOfProduts = _unitOfWork.Product.GetActiveProducts().Count(),
-                NumberOfNewsletterSubscribers = newsletterSubscribers
+                NumberOfProduts = _unitOfWork.Product.GetActiveProducts().Count()
+                //NumberOfNewsletterSubscribers = newsletterSubscribers
             };
 
             return View(model);
